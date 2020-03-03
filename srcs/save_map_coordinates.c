5a95f33b136e0098f69a4fc67ed6be4cca973380
Jessica D. Smith <jesmith@student.codam.nl>
2020-03-03 18:20:58 +0100

    almost save_map_coordinates.c
    slight issue with adding extra length to x axis

--- srcs/save_map_coordinates.c
+++ srcs/save_map_coordinates.c
@@ -6,9 +6,11 @@
 /*   By: mminkjan <mminkjan@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/08 14:14:17 by mminkjan       #+#    #+#                */
-/*   Updated: 2020/03/03 16:02:25 by jesmith       ########   odam.nl         */
+/*   Updated: 2020/03/03 18:17:55 by jesmith       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../includes/wolf3d.h"
 
 #include "../includes/wolf3d.h"
 
@@ -21,10 +23,7 @@ static void	save_vertical_item(t_wolf *wolf, t_point start,
 	if (item == NULL)
 		wolf_failure_exit(wolf, map_values, MALLOC_ERR);
 	item->start = start;
-	if (i->y + 1 == wolf->max_y)
-		item->end.y = (i->y - 1) * wolf->module;
-	else
-		item->end.y = i->y * wolf->module;
+	item->end.y = i->y * wolf->module;
 	item->end.x = start.x;
 	item->texture = map_values[i->y][i->x];
 	item->next = NULL;
@@ -39,9 +38,7 @@ static void	vertical_coordinates(t_wolf *wolf, int **values, t_i *i)
 	i->y = 0;
 	hold_y = 0;
 	ft_bzero(&start, sizeof(t_point));
-	if ((i->x + 1 == wolf->max_x) || (i->x != 1 && i->x != 0))
-		start.x = (i->x - 1) * wolf->module;
-	else
+	if (i->x != 0)
 		start.x = i->x * wolf->module;
 	while (i->y < wolf->max_y)
 	{
@@ -69,8 +66,8 @@ static void	save_horizontal_item(t_wolf *wolf, t_point start,
 	if (item == NULL)
 		wolf_failure_exit(wolf, map_values, MALLOC_ERR);
 	item->start = start;
-	if (i->x + 1 == wolf->max_x)
-		item->end.x = (i->x - 1) * wolf->module;
+	if (i->y != 0)// && i->x + 1 != wolf->max_x)
+		item->end.x = (i->x + 1) * wolf->module;
 	else if (map_values[i->y][i->x] > 4)
 		item->end.x = (i->x + 1) * 70;
 	else
@@ -87,24 +84,21 @@ static void	horizontal_coordinates(t_wolf *wolf, int **values, t_i *i)
 
 	i->x = 0;
 	ft_bzero(&start, sizeof(t_point));
-	if (i->y != 0 && i->y + 1 != wolf->max_y)
+	if (i->y != 0)
 		start.y = i->y * wolf->module;
-	if (i->y + 1 == wolf->max_y)
-		start.y = (i->y - 1) * wolf->module;
 	while (i->x < wolf->max_x - 1)
 	{
 		if (values[(int)i->y][(int)i->x] != 0)
 		{
 			while (values[i->y][i->x + 1] == values[i->y][i->x])
 				i->x++;
-			if ((values[i->y][i->x - 1] < 5 && values[i->y][i->x - 1] > 0 \
-			&& values[i->y][i->x + 1] >= 0) || \
-			(values[i->y][i->x + 1] > 0 && values[i->y][i->x + 1] < 5))
-				save_horizontal_item(wolf, start, i, values);
+			if ((values[i->y][i->x + 1] > 0 && values[i->y][i->x + 1] < 5) || (i->x + 1 == wolf->max_x))
+				if ((i->y + 1 < wolf->max_y && values[i->y + 1][i->x] == 0) || (i->y + 1 == wolf->max_y) || (i->y == 0))
+					save_horizontal_item(wolf, start, i, values);
 		}
-		start.x = i->x * wolf->module;
 		if (i->y != 0)
 			i->x++;
+		start.x = i->x * wolf->module;
 	}
 }
 
