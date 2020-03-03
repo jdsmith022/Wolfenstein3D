c3478b3227f38e1fefbf402f22292e33e3945bdd
Manon louise Minkjan <mminkjan@f1r1s12.codam.nl>
2020-03-03 13:00:36 +0100

    MSG

--- srcs/key_events.c
+++ srcs/key_events.c
@@ -6,7 +6,7 @@
 /*   By: mminkjan <mminkjan@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/10 19:09:56 by mminkjan       #+#    #+#                */
-/*   Updated: 2020/03/03 12:43:48 by mminkjan      ########   odam.nl         */
+/*   Updated: 2020/03/03 12:58:52 by mminkjan      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,54 @@
 
 static t_point	check_collision(t_wolf *wolf, int key)
 {
-	t_point pos;
-	t_point hold;
+	
 
 	pos = wolf->pos;
 	hold = wolf->pos;
-
 	if (key == W)
 	{
 		pos.x += SPEED * cos(wolf->dir_angle);
 		pos.y += SPEED * sin(wolf->dir_angle);
-		if (wolf->map[(int)pos.y / wolf->module][(int)pos.x / wolf->module] == 0)
-			return (pos);
 	}
+	else if (key == S)
+	{
+		pos.x -= SPEED * cos(wolf->dir_angle);
+		pos.y -= SPEED * sin(wolf->dir_angle);if 
+
+	}
+	if (wolf->map[(int)pos.y / wolf->module][(int)pos.x / wolf->module] != 0)
+		return (hold);
+	return (pos);
+
 }
 
 static void	key_player_position(t_wolf *wolf, int key)
 {
-	wolf->event.hold_angle = wolf->dir_angle;
+	t_point pos;
+
+	pos = wolf->pos;
 	if (key == W)
 	{
-		wolf->pos = check_collision(wolf, key);
-		// wolf->pos.x += SPEED * cos(wolf->dir_angle);
-		// wolf->pos.y += SPEED * sin(wolf->dir_angle);
+		pos.x += SPEED * cos(wolf->dir_angle);
+		pos.y += SPEED * sin(wolf->dir_angle);
 	}
 	if (key == S)
 	{
-		wolf->pos.x -= SPEED * cos(wolf->dir_angle);
-		wolf->pos.y -= SPEED * sin(wolf->dir_angle);
+		pos.x -= SPEED * cos(wolf->dir_angle);
+		pos.y -= SPEED * sin(wolf->dir_angle);
 	}
 	if (key == A && wolf->pos.x >= 10)
 	{
-		wolf->pos.x += SPEED * cos(wolf->dir_angle - (90 * PI / 180));
-		wolf->pos.y += SPEED * sin(wolf->dir_angle - (90 * PI / 180));
+		pos.x += SPEED * cos(wolf->dir_angle - (90 * PI / 180));
+		pos.y += SPEED * sin(wolf->dir_angle - (90 * PI / 180));
 	}
 	else if (key == D)
 	{
-		wolf->pos.x += SPEED * cos(wolf->dir_angle + (90 * PI / 180));
-		wolf->pos.y += SPEED * sin(wolf->dir_angle + (90 * PI / 180));
+		pos.x += SPEED * cos(wolf->dir_angle + (90 * PI / 180));
+		pos.y += SPEED * sin(wolf->dir_angle + (90 * PI / 180));
 	}
+	if (wolf->map[(int)pos.y / wolf->module][(int)pos.x / wolf->module] != 0)
+		wolf->pos = pos;
 }
 
 static void	key_window_handling(t_wolf *wolf, int key)
