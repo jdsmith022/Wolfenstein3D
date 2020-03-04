0f9bb4a47ea5e9feb4af193e9d51a281cb9c2ced
Jessica D. Smith <jesmith@student.codam.nl>
2020-03-04 13:07:58 +0100

    save_map_coordinates
    need to tweak most likely as we go along
    
    too many lines in horizontal_coordinates
    
    see if we can make prettier

--- srcs/save_map_coordinates.c
+++ srcs/save_map_coordinates.c
@@ -6,11 +6,9 @@
 /*   By: mminkjan <mminkjan@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/08 14:14:17 by mminkjan       #+#    #+#                */
-/*   Updated: 2020/03/04 09:58:54 by jesmith       ########   odam.nl         */
+/*   Updated: 2020/03/04 13:07:13 by jesmith       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "../includes/wolf3d.h"
 
 #include "../includes/wolf3d.h"
 
@@ -23,7 +21,10 @@ static void	save_vertical_item(t_wolf *wolf, t_point start,
 	if (item == NULL)
 		wolf_failure_exit(wolf, map_values, MALLOC_ERR);
 	item->start = start;
-	item->end.y = i->y * wolf->module;
+	if (i->y + 1 == wolf->max_y)
+		item->end.y = (i->y - 1) * wolf->module;
+	else
+		item->end.y = i->y * wolf->module;
 	item->end.x = start.x;
 	item->texture = map_values[i->y][i->x];
 	item->next = NULL;
@@ -33,24 +34,28 @@ static void	save_vertical_item(t_wolf *wolf, t_point start,
 static void	vertical_coordinates(t_wolf *wolf, int **values, t_i *i)
 {
 	t_point start;
+	int		hold_y;
 
 	i->y = 0;
+	hold_y = 0;
 	ft_bzero(&start, sizeof(t_point));
-	if (i->x != 0)
+	if ((i->x + 1 == wolf->max_x) || (i->x > 1))
+		start.x = (i->x - 1) * wolf->module;
+	else
 		start.x = i->x * wolf->module;
 	while (i->y < wolf->max_y)
 	{
 		if (values[i->y][i->x] > 0 && values[i->y][i->x] < 5)
 		{
-			while (i->y + 1 < wolf->max_y && values[i->y + 1][i->x] == values[i->y][i->x])
+			while (i->y + 1 < wolf->max_y && \
+			values[i->y + 1][i->x] == values[i->y][i->x])
 				i->y++;
-			if ((i->y + 1 < wolf->max_y && i->y - 1 >= 0 &&  values[i->y + 1][i->x] < 5 && values[i->y - 1][i->x] > 0) || (i->x == 0) || (i->x + 1 == wolf->max_x))
-			// if ((i->y + 1 < wolf->max_y && i->y - 1 > 0 && \
-			// 	values[i->y + 1][i->x] < 5 && values[i->y - 1][i->x] > 0) || \
-			// 	(i->y + 1 == wolf->max_y && values[i->y - 1][i->x] != 0) || \
-			// 	(i->y - 1 == 0 && values[i->y + 1][i->x] > 0))
+			if ((i->x == 0) || (i->y + 1 < wolf->max_y && i->y - 1 >= 0 && \
+				values[i->y + 1][i->x] < 5 && values[i->y - 1][i->x] > 0) || \
+				(i->x + 1 == wolf->max_x))
 				save_vertical_item(wolf, start, i, values);
 		}
+		hold_y = i->y;
 		start.y = i->y * wolf->module;
 		i->y++;
 	}
@@ -65,10 +70,12 @@ static void	save_horizontal_item(t_wolf *wolf, t_point start,
 	if (item == NULL)
 		wolf_failure_exit(wolf, map_values, MALLOC_ERR);
 	item->start = start;
-	if (map_values[i->y][i->x] > 4)
+	if (i->x + 1 == wolf->max_x)
+		item->end.x = (i->x - 1) * wolf->module;
+	else if (map_values[i->y][i->x] > 4)
 		item->end.x = (i->x + 1) * 70;
 	else
-		item->end.x = (i->x + 1) * wolf->module;
+		item->end.x = i->x * wolf->module;
 	item->end.y = start.y;
 	item->texture = map_values[i->y][i->x];
 	item->next = NULL;
@@ -81,22 +88,28 @@ static void	horizontal_coordinates(t_wolf *wolf, int **values, t_i *i)
 
 	i->x = 0;
 	ft_bzero(&start, sizeof(t_point));
-	if (i->y != 0)
+	if (i->y != 0 && i->y + 1 != wolf->max_y)
 		start.y = i->y * wolf->module;
+	if (i->y + 1 == wolf->max_y)
+		start.y = (i->y - 1) * wolf->module;
 	while (i->x < wolf->max_x - 1)
 	{
 		if (values[(int)i->y][(int)i->x] != 0)
 		{
 			while (values[i->y][i->x + 1] == values[i->y][i->x])
 				i->x++;
-		if ((values[i->y][i->x + 1] > 0 && values[i->y][i->x + 1] < 5) || (i->x + 1 == wolf->max_x) || (i->x != 0))
+			if ((values[i->y][i->x + 1] > 0 && values[i->y][i->x + 1] < 5) \
+				|| (i->x + 1 == wolf->max_x) || (i->x != 0))
 			{
-				if ((i->y + 1 < wolf->max_y && values[i->y + 1][i->x] == 0 && i->y - 1 > 0 && values[i->y - 1][i->x] == 0) || (i->y + 1 == wolf->max_y) || (i->y == 0))
+				if ((i->y + 1 < wolf->max_y && values[i->y + 1][i->x] == 0 \
+					&& values[i->y][i->x + 1] != 0) || \
+					(i->x == 1 && values[i->y - 1][i->x] == 0))
 					save_horizontal_item(wolf, start, i, values);
 			}
 		}
-		i->x++;
 		start.x = i->x * wolf->module;
+		if (i->y != 0)
+			i->x++;
 	}
 }
 
