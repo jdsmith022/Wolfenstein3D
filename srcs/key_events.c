99c443927155bec56d7cf02d5d718610cda377a0
Jessica D. Smith <jesmith@student.codam.nl>
2020-03-09 13:01:28 +0100

    working wall collision
    need to do further testing
    
    key_events is 27 lines

--- srcs/key_events.c
+++ srcs/key_events.c
@@ -6,13 +6,37 @@
 /*   By: mminkjan <mminkjan@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/10 19:09:56 by mminkjan       #+#    #+#                */
-/*   Updated: 2020/03/03 16:00:44 by jesmith       ########   odam.nl         */
+/*   Updated: 2020/03/09 12:59:49 by jesmith       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/wolf3d.h"
 
-static void	key_player_position(t_wolf *wolf, int key)
+static int		intersect_direction(t_wolf *wolf, t_point pos)
+{
+	double		angle;
+	t_item		ray;
+	int			x;
+
+	x = 0;
+	angle = wolf->dir_angle - (FOV / 2);
+	ray.start.x = pos.x;
+	ray.start.y = pos.y;
+	while (x < WIDTH)
+	{
+		angle = clamp_angle(angle);
+		ray.end.x = ray.start.x + wolf->max_ray * cos(angle);
+		ray.end.y = ray.start.y + wolf->max_ray * sin(angle);
+		wolf->intersect = find_intersect(wolf, ray, wolf->height, angle);
+		angle += wolf->ray_angle;
+		x++;
+	}
+	if (wolf->intersect.obj_dist < 20)
+		return (1);
+	return (0);
+}
+
+static void		key_player_position(t_wolf *wolf, int key)
 {
 	t_point pos;
 	double	radian;
@@ -39,25 +63,24 @@ static void	key_player_position(t_wolf *wolf, int key)
 		pos.x += SPEED * cos(wolf->dir_angle + radian);
 		pos.y += SPEED * sin(wolf->dir_angle + radian);
 	}
-	printf("pos: %d     %d, %d\n", wolf->map[(int)round(pos.y / wolf->module)][(int)round(pos.x / wolf->module)], (int)round(pos.x / wolf->module), (int)round(pos.y / wolf->module));
-	if (wolf->map[(int)round(pos.y / wolf->module)][(int)round(pos.x / wolf->module)] == 0)
+	if (intersect_direction(wolf, pos) == 0)
 		wolf->pos = pos;
 }
 
-static void	key_window_handling(t_wolf *wolf, int key)
+static void		key_window_handling(t_wolf *wolf, int key)
 {
 	if (key == ESC)
 		wolf_success_exit(wolf);
 }
 
-static int			key_events(int key, t_wolf *wolf)
+static int		key_events(int key, t_wolf *wolf)
 {
 	key_window_handling(wolf, key);
 	key_player_position(wolf, key);
 	return (0);
 }
 
-void		mlx_key(t_wolf *wolf)
+void			mlx_key(t_wolf *wolf)
 {
 	mlx_hook(wolf->win_ptr, 2, 0, key_events, wolf);
 	mlx_hook(wolf->win_ptr, 17, 0, wolf_success_exit, wolf);
