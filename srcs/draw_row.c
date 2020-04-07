3da538e88479f44df8dbce3bcc5bd4c37b36acbc
Jessica Smith <JessicaSmith@Jessies-MacBook-Pro.local>
2020-04-07 16:22:57 +0200

    updated row
    still have issue
    
    calcualting steps I think

--- srcs/draw_row.c
+++ srcs/draw_row.c
@@ -6,7 +6,7 @@
 /*   By: JessicaSmith <JessicaSmith@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/06 11:07:45 by JessicaSmit   #+#    #+#                 */
-/*   Updated: 2020/04/06 11:43:59 by JessicaSmit   ########   odam.nl         */
+/*   Updated: 2020/04/07 16:22:32 by JessicaSmit   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,14 @@ static void	put_pixel(t_wolf *wolf, int color, int x, int y)
 	}
 }
 
-static void	put_row(t_wolf *wolf, int x, int y, double text_x, double text_y)
+static void	put_row(t_wolf *wolf, int x, int y, t_i text)
 {
 	size_t addr_dex;
 	size_t wall_dex;
 	size_t row_dex;
 
 	addr_dex = 4 * (WIDTH * y + x);
-	wall_dex = 4 * (64 * (int)text_y + (int)text_x);
+	wall_dex = 4 * (64 * (int)text.y + (int)text.x);
 	row_dex = wolf->graphics.row_dex;
 	wolf->graphics.addr_str[addr_dex] = \
 		wolf->graphics.wall[row_dex]->addr_str[wall_dex];
@@ -49,28 +49,22 @@ static void	put_row(t_wolf *wolf, int x, int y, double text_x, double text_y)
 		wolf->graphics.wall[row_dex]->addr_str[wall_dex];
 }
 
- void	draw_row(t_wolf *wolf, t_item ray, int y, int x)
+void	draw_row(t_wolf *wolf, t_item ray, int y, int x)
 {
-
-	double  dist;
+	double	dist;
 	double	floor_x;
 	double	floor_y;
-	double	text_x;
-	double	text_y;
-    double  ray_dist;
-
-    ray_dist = (32 / (y - HEIGHT / 2)) * wolf->dist_to_plane;
-    ray_dist /= cos(wolf->angle);
+	t_i		text;
 
 	if (wolf->event.colors == 1)
 		put_pixel(wolf, 0xd57016, x, y);
 	else
 	{
-		dist = (double)HEIGHT /(2 * (double)y - (double)HEIGHT);
-		floor_x = dist / ray_dist * ray.end.x + (1 - (dist / ray_dist)) * wolf->pos.x;
-		floor_y = dist / ray_dist * ray.end.y + (1 - (dist / ray_dist)) * wolf->pos.y;
-		text_x = (int)(floor_x * 64) % 64;
-		text_y = (int)(floor_y * 64) % 64;
-		put_row(wolf, x, y, text_x, text_y);
+		dist = (double)HEIGHT / (2 * (double)y - (double)HEIGHT);
+		floor_x = dist / wolf->dist_to_plane * ray.end.x + (1 - (dist / wolf->dist_to_plane)) * wolf->pos.x;
+		floor_y = dist / wolf->dist_to_plane * ray.end.y + (1 - (dist / wolf->dist_to_plane)) * wolf->pos.y;
+		text.x = (int)(floor_x * 64) % 64;
+		text.y = (int)(floor_y * 64) % 64;
+		put_row(wolf, x, y, text);
 	}
 }
