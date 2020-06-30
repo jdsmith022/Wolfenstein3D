74500cda6aa34ea98291ec7254907f61ce3a4284
Manon louise Minkjan <mminkjan@f1r1s12.codam.nl>
2020-06-30 17:04:33 +0200

    valid input main

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,26 @@
 /*   By: mminkjan <mminkjan@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/03 20:21:28 by mminkjan      #+#    #+#                 */
-/*   Updated: 2020/06/30 16:54:57 by jesmith       ########   odam.nl         */
+/*   Updated: 2020/06/30 17:03:17 by mminkjan      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/wolf3d.h"
+
+void	check_valid_input(int argc, char *filename, char *exe)
+{
+	if (argc != 2)
+	{
+		ft_putendl(USAGE_ERR);
+		exit(EXIT_FAILURE);
+	}
+	if (ft_strcmp(exe, filename) == 0 || \
+		ft_strcmp(&exe[2], filename) == 0)
+	{
+		ft_putendl(USAGE_ERR);
+		exit(EXIT_FAILURE);
+	}
+}
 
 int		main(int argc, char **argv)
 {
@@ -20,14 +35,9 @@ int		main(int argc, char **argv)
 
 	filename = argv[1];
 	exe = argv[0];
+	check_valid_input(argc, filename, exe);
 	if (ft_strcmp(argv[1], "--help") == 0)
 		print_help_menu();
-	if (argc != 2 || ft_strcmp(exe, filename) == 0 || \
-		ft_strcmp(&exe[2], filename) == 0)
-	{
-		ft_putendl(USAGE_ERR);
-		exit(EXIT_FAILURE);
-	}
 	wolf = init_wolf();
 	wolf.map = save_map_values(&wolf, argv[1]);
 	save_map_coordinates(&wolf);
